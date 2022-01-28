72b28b1c35309e0e54ec82cd679b6adb36a9afe8
Viktoriia Huk <relax16011991@gmail.com>
2022-01-29 00:00:36 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import { useState, useEffect } from "react";
 import Searchbar from "./components/Searchbar/Searchbar";
 import ImageGallery from "./components/ImageGallery/ImageGallery";
 import Button from "./components/Button/Button";
@@ -7,120 +7,101 @@ import fetchImages from "./services/images-api";
 import Modal from "./components/Modal/Modal";
 import "./App.css";
 
-class App extends Component {
-  state = {
-    searchQuery: "",
-    images: [],
-    page: 1,
-    showModal: false,
-    loadMore: false,
-    largeImageURL: "",
-    status: "idle",
-  };
+export default function App () {
 
-  componentDidUpdate(prevProps, prevState) {
-    const prevQuery = prevState.searchQuery;
-    const nextQuery = this.state.searchQuery;
-    const { page } = this.state;
-    if (prevQuery !== nextQuery) {
-      this.setState({ images: [], page: 1, status: "pending" });
+  const[searchQuery, setSearchQuery] = useState('');
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [showModal, setShowModal] = useState(false);
+  const [loadMore, setLoadMore] = useState(false);
+  const [largeImageURL, setLargeImageURL] = useState('');
+  const[status, setStatus] = useState('idle');
 
-      this.getImgFromFetch(nextQuery, page);
+
+   useEffect(() => {
+    if (!searchQuery) {
+      return;
     }
-    if (prevState.page !== page && page !== 1) {
-      this.getImgFromFetch(nextQuery, page);
-    }
-  }
-
-  getImgFromFetch = (searchQuery, pageNumber) => {
-    fetchImages(searchQuery, pageNumber)
-      .then((images) => {
-        if (!images.hits.length) {
-          alert("Картинка не найдена");
-          this.setState({
-            error: "Ошибка, попробуйте еще раз",
-            status: "rejected",
-          });
-        } else {
-          const data = images.hits.map(
-            ({ id, tags, webformatURL, largeImageURL }) => {
-              return {
-                id,
-                webformatURL,
-                tags,
-                largeImageURL,
-              };
-            }
-          );
-          this.setState((prevState) => ({
-            images: [...prevState.images, ...data],
-            status: "resolved",
-            loadMore: true,
-          }));
-        }
+     fetchImages(searchQuery, page)
+       .then((images) => {
+         if (!images.hits.length) {
+           alert("Картинка не найдена");
+           setStatus('rejected');
+           return;
+         } else {
+           const data = images.hits.map(
+             ({ id, tags, webformatURL, largeImageURL }) => {
+               return {
+                 id,
+                 webformatURL,
+                 tags,
+                 largeImageURL,
+               };
+             });
+           setImages(state => [...state, ...data]);
+           setStatus('resolved');
+           setLoadMore(true);
+         };
+       
         window.scrollTo({
           top: document.documentElement.scrollHeight,
           behavior: "smooth",
         });
       })
-      .catch((error) =>
-        this.setState({
-          error,
-          status: "rejected",
-        })
-      );
+      .catch((error) => setStatus("rejected")
+       );
+  }, [searchQuery, page]);
+
+  const handleFormSubmit = searchQuery => {
+    setSearchQuery(searchQuery);
+    setPage(1);
+    setImages([]);
+    setStatus('idle');
   };
 
-  handleFormSubmit = (searchQuery) => {
-    this.setState({ searchQuery, page: 1 });
+  const onClickButton = () => {
+    setPage(page => page + 1);
+    setStatus("pending")
+   
   };
 
-  onClickButton = () => {
-    this.setState((prevState) => ({
-      page: prevState.page + 1,
-      status: "pending",
-    }));
+  const toggleModal = () => {
+    setShowModal(showModal =>!showModal);
   };
 
-  toggleModal = () => {
-    this.setState(({ showModal }) => ({
-      showModal: !showModal,
-    }));
+  const onImageClick = (largeImageURL) => {
+    setLargeImageURL( largeImageURL);
+    toggleModal();
   };
 
-  onImageClick = (largeImageURL) => {
-    this.setState({ largeImageURL });
-    this.toggleModal();
-  };
-
-  render() {
-    const { images, status, showModal, largeImageURL, loadMore } = this.state;
+  
+    
     return (
       <div>
-        <Searchbar onSubmit={this.handleFormSubmit} />
+        <Searchbar onSubmit={handleFormSubmit} />
 
         {status === "pending" && (
           <>
-            <ImageGallery images={images} onImageClick={this.onImageClick} />
+            <ImageGallery images={images} onImageClick={onImageClick} />
             <Loader />
           </>
         )}
 
         {status === "resolved" && (
           <>
-            <ImageGallery images={images} onImageClick={this.onImageClick} />
-            {loadMore && <Button loadMore={this.onClickButton} />}
+            <ImageGallery images={images} onImageClick={onImageClick} />
+            {loadMore && <Button loadMore={onClickButton} />}
           </>
         )}
 
         {showModal && (
-          <Modal onClose={this.toggleModal}>
+          <Modal onClose={toggleModal}>
             <img src={largeImageURL} alt="" />
           </Modal>
         )}
       </div>
     );
-  }
+  
 }
 
-export default App;
+
